22bb1741ff8f3cd1fee03e12d4785c96b982aaa3
Nicholas Krebs <nicholas@nicholas-krebs.ch>
2025-05-06 15:48:22 +0200

    #35 match theme hadron v2.1.0

--- themes/local/local.php
+++ themes/local/local.php
@@ -1,9 +1,22 @@
 <?php
-    namespace Grav\Theme;
+	namespace Grav\Theme;
 
-    use Grav\Common\Theme;
+	use Grav\Common\Grav;
+	use Grav\Common\Theme;
 
-    class Local extends Hadron
-    {
-        // Access plugin events in this class
-    }
+	class Local extends Hadron {
+		public static function getSubscribedEvents() {
+			return [
+				'onTwigLoader' => ['onTwigLoader', 0],
+			];
+		}
+
+		public function onTwigLoader() {
+			parent::onTwigLoader();
+
+			// add parent theme as namespace to twig
+			$parentThemeName = 'hadron';
+			$parentThemePath = Grav::instance()['locator']->findResource('themes://' . $parentThemeName);
+			$this->grav['twig']->addPath($parentThemePath . DIRECTORY_SEPARATOR . 'templates', $parentThemeName);
+		}
+	}
